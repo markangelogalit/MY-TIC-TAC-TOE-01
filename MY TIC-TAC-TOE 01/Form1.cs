fd3b1f947077c8504848c0120067482c0b29be94
Mark Angelo Galit <rankm12345@gmail.com>
2022-06-04 00:58:36 +0800

    adding 9 buttons

--- MY TIC-TAC-TOE 01/Form1.cs
+++ MY TIC-TAC-TOE 01/Form1.cs
@@ -6,5 +6,10 @@ namespace MY_TIC_TAC_TOE_01
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
