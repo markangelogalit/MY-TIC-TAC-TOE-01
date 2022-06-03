905f963687eef50faee0c9fe20c2bd1f8856bbc8
Mark Angelo Galit <rankm12345@gmail.com>
2022-06-04 01:00:47 +0800

    clearing 9 buttons name

--- MY TIC-TAC-TOE 01/Form1.Designer.cs
+++ MY TIC-TAC-TOE 01/Form1.Designer.cs
@@ -53,7 +53,6 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(107, 72);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -63,8 +62,8 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(107, 72);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -72,7 +71,6 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(107, 72);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
@@ -81,7 +79,6 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(107, 72);
             this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
@@ -90,7 +87,6 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(107, 72);
             this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
@@ -99,8 +95,8 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(107, 72);
             this.button7.TabIndex = 6;
-            this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -108,16 +104,14 @@
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(107, 72);
             this.button8.TabIndex = 7;
-            this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(241, 200);
+            this.button9.Location = new System.Drawing.Point(238, 200);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(107, 72);
             this.button9.TabIndex = 8;
-            this.button9.Text = "button9";
             this.button9.UseVisualStyleBackColor = true;
             // 
             // Form1
--- MY TIC-TAC-TOE 01/Form1.cs
+++ MY TIC-TAC-TOE 01/Form1.cs
@@ -11,5 +11,15 @@ namespace MY_TIC_TAC_TOE_01
         {
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
